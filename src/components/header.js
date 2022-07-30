38ef9fa0b81c1aa7227d5f2ae1df3c2c231f2e4e
Alexandra Spalato <alexaspalato@gmail.com>
2022-07-30 20:25:18 +0200

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,27 @@
-import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import { FaShoppingCart } from "react-icons/fa"
+
+import logo from "../images/logo.svg"
 
 const Header = ({ siteTitle }) => (
-  <header className="bg-slate-200">
-    <div className=" container mx-auto py-8">
-      <Link to="/">{siteTitle}</Link>
+  <header className="bg-purple-700 py-3">
+    <div className="container mx-auto flex justify-between items-center">
+      <div className="navbar-brand ">
+        <Link to="/" className="navbar-item">
+          <img
+            style={{ height: 60, maxHeight: "none", marginBottom: 0 }}
+            src={logo}
+            alt="Level Up Logo"
+          />
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <FaShoppingCart style={{ color: "white", height: 30, width: 30 }} />
+        </div>
+      </div>
     </div>
   </header>
 )
