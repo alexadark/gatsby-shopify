d75d1502703e585a44f26d58f2c72501610f8623
alexandra spalato <alexaspalato@gmail.com>
2022-08-01 16:28:12 +0200

    cart total and images

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,8 +4,9 @@ import { StoreContext } from "../../context/StoreContext"
 const Cart = () => {
   const { checkout, toggleCartOpen } = useContext(StoreContext)
   console.log("checkout", checkout.lineItems)
+
   return (
-    <div className="fixed top-0 right-0 z-10 w-1/2 h-full bg-white shadow-2xl">
+    <div className="fixed top-0 right-0 z-10 w-1/2 h-full p-5 bg-white shadow-2xl">
       <div className="flex justify-end">
         <button className="p-3" onClick={toggleCartOpen}>
           close
@@ -16,12 +17,22 @@ const Cart = () => {
 
         return (
           <div key={item.id}>
-            <h4>{item.title}</h4>
-            <p>{item.variant.price}$</p>
+            <div className="flex items-center gap-5">
+              <img src={item.variant.image.src} alt="" className="h-[75px]" />
+              <h4>{item.title}</h4>
+            </div>
+            <p>
+              {item.variant.priceV2.amount}
+              {item.variant.priceV2.currencyCode}
+            </p>
             <p>{item.quantity}</p>
           </div>
         )
       })}
+      <p>
+        Total: {checkout.totalPriceV2.amount}
+        {checkout.totalPriceV2.currencyCode}
+      </p>
     </div>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,19 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useContext, useState } from "react"
+import React, { useContext } from "react"
 import { FaShoppingCart } from "react-icons/fa"
 import { StoreContext } from "../context/StoreContext"
 import Cart from "./cart/Cart"
 
-import logo from "../images/logo.svg"
-
 const Header = ({ siteTitle }) => {
-  const { cart, addProductToCart, client, isCartOpen, toggleCartOpen } =
-    useContext(StoreContext)
-  // const [isCartOpen, setCartOpen] = useState(false)
+  const { isCartOpen, toggleCartOpen } = useContext(StoreContext)
+
   return (
     <header className="py-3 bg-purple-700">
-      <div className="container flex items-center justify-between mx-auto">
-        <div className="navbar-brand ">
-          <Link to="/" className="navbar-item">
-            <img
-              style={{ height: 60, maxHeight: "none", marginBottom: 0 }}
-              src={logo}
-              alt="Level Up Logo"
-            />
+      <div className="container flex items-center justify-between px-5 mx-auto">
+        <div className=" navbar-brand">
+          <Link to="/" className="font-bold text-white uppercase navbar-item">
+            Gatsby-shopify
           </Link>
         </div>
         <div className="navbar-end">
